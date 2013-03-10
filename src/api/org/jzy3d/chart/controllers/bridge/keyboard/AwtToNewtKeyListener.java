d3d96eacc1ba25ba967443f14057857138b76b71
nilshoffmann <>
2013-03-10 20:21:02 +0100

    Removed leftover file header template. Fixed missing newline.

--- src/api/org/jzy3d/chart/controllers/bridge/keyboard/AwtToNewtKeyListener.java
+++ src/api/org/jzy3d/chart/controllers/bridge/keyboard/AwtToNewtKeyListener.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.jzy3d.chart.controllers.bridge.keyboard;
 
 import com.jogamp.newt.event.KeyEvent;
