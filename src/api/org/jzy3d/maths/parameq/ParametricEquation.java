bbc866eb3526160aad09df0f039dbc3b4d6589b0
Martin Pernollet <martin.pernollet@gmail.com.com>
2013-04-03 12:43:03 +0200

    added drawable parametric equation, improve maven build file

---
+++ src/api/org/jzy3d/maths/parameq/ParametricEquation.java
@@ -0,0 +1,19 @@
+package org.jzy3d.maths.parameq;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jzy3d.maths.Coord3d;
+
+public abstract class ParametricEquation {
+    public List<Coord3d> apply(double tmin, double tmax, int steps){
+        List<Coord3d> coords = new ArrayList<Coord3d>(steps);
+        double step = (tmax-tmin)/steps;
+        for(int t=0; t<steps; t++){
+            double value = tmin + t*step;
+            coords.add(apply(value));
+        }
+        return coords;
+    }
+    public abstract Coord3d apply(double t);
+}
---
+++ src/api/org/jzy3d/maths/parameq/ParametricEquation2.java
@@ -0,0 +1,24 @@
+package org.jzy3d.maths.parameq;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jzy3d.maths.Coord3d;
+
+public abstract class ParametricEquation2 {
+    public List<Coord3d> apply(double tmin, double tmax, int tsteps, double umin, double umax, int usteps) {
+        List<Coord3d> coords = new ArrayList<Coord3d>(tsteps);
+        double tstep = (tmax - tmin) / tsteps;
+        double ustep = (umax - umin) / usteps;
+        for (int t = 0; t < tsteps; t++) {
+            double tvalue = tmin + t * tstep;
+            for (int u = 0; u < usteps; u++) {
+                double uvalue = umin + u * ustep;
+                coords.add(apply(tvalue, uvalue));
+            }
+        }
+        return coords;
+    }
+
+    public abstract Coord3d apply(double t, double u);
+}
