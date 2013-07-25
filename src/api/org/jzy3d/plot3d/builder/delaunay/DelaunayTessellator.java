ed6958daf81b0fd7c37e997ddeeb2710b37a7d42
Martin Pernollet <martin.pernollet@gmail.com.com>
2013-07-25 11:35:10 +0200

    getting delaunay triangulation through jdt-core fork

--- src/api/org/jzy3d/plot3d/builder/delaunay/DelaunayTessellator.java
+++ src/api/org/jzy3d/plot3d/builder/delaunay/DelaunayTessellator.java
@@ -1,13 +1,14 @@
 package org.jzy3d.plot3d.builder.delaunay;
 
+import il.ac.idc.jdt.DelaunayTriangulation;
+import il.ac.idc.jdt.Triangle;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.jzy3d.maths.Coord3d;
 import org.jzy3d.plot3d.builder.Tessellator;
-import org.jzy3d.plot3d.builder.delaunay.jdt.Delaunay_Triangulation;
-import org.jzy3d.plot3d.builder.delaunay.jdt.Triangle_dt;
 import org.jzy3d.plot3d.primitives.AbstractComposite;
 import org.jzy3d.plot3d.primitives.Polygon;
 import org.jzy3d.plot3d.primitives.Shape;
@@ -20,30 +21,27 @@ public class DelaunayTessellator extends Tessellator{
     }
 
     protected List<Polygon> computePolygons(List<Coord3d> coordinates) {
-        // append all input Jzy3d coordinates in JDT triangulator
-        Delaunay_Triangulation triangulator = new Delaunay_Triangulation();
+        // Append all input Jzy3d coordinates in JDT triangulator
+        DelaunayTriangulation triangulator = new DelaunayTriangulation();
         for (Coord3d coord: coordinates) {
-            triangulator.insertPoint(JDTTypeConverter.toJdtPoint(coord));
+            triangulator.insertPoint(JDTConverter.toJdtPoint(coord));
         }
 
         // Retrieve triangles computed by JDT
         List<Polygon> polygons = new ArrayList<Polygon>(triangulator.trianglesSize());
-        // TODO: three different point classes coord3d, point_dt !!
-
-        Iterator<Triangle_dt> it = triangulator.trianglesIterator();
+        Iterator<Triangle> it = triangulator.trianglesIterator();
         while (it.hasNext()) {
-            Triangle_dt triangle = it.next();
-
-            if (triangle.isHalfplane()) { /* isHalfplane means a degenerated triangle */
+            Triangle triangle = it.next();
+            // isHalfplane indicates a degenerated triangle
+            if (triangle.isHalfplane()) 
                 continue;
-            }
-            polygons.add(JDTTypeConverter.toJzyPolygon(triangle));
+            polygons.add(JDTConverter.toJzyPolygon(triangle));
         }
         return polygons;
     }
 
     @Override
     public AbstractComposite build(float[] x, float[] y, float[] z) {
-        throw new RuntimeException("not implemented");
+        throw new RuntimeException("not called");
     }
 }
