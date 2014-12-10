9fcbaf6ec25c7350309bcc9a3fc97141bf74cdd6
andrea <bedinandrea.96@gmail.com>
2014-12-10 21:40:34 +0100

    working axis tick

--- jzy3d-api/src/api/org/jzy3d/plot3d/primitives/axeTransformablePrimitive/axeTransformableAxes/AxeTransformableAxeBox.java
+++ jzy3d-api/src/api/org/jzy3d/plot3d/primitives/axeTransformablePrimitive/axeTransformableAxes/AxeTransformableAxeBox.java
@@ -137,7 +137,7 @@ public class AxeTransformableAxeBox extends AxeBox {
             }
         }
     }
-
+/*
     public void drawAxisLabel(GL gl, GLU glu, Camera cam, int direction, Color color, BoundingBox3d ticksTxtBounds, double xlab, double ylab, double zlab, String axeLabel) {
         if (isXDisplayed(direction) || isYDisplayed(direction) || isZDisplayed(direction)) {
             Coord3d labelPosition = new Coord3d(transformers.getX().compute((float)xlab), transformers.getY().compute((float)ylab), transformers.getZ().compute((float)zlab));
@@ -146,7 +146,6 @@ public class AxeTransformableAxeBox extends AxeBox {
                 ticksTxtBounds.add(labelBounds);
         }
     }
-
     public void drawAxisTickNumericLabel(GL gl, GLU glu, int direction, Camera cam, Color color, Halign hAlign, Valign vAlign, BoundingBox3d ticksTxtBounds, String tickLabel,
             Coord3d tickPosition) {
         //doTransform(gl);
@@ -162,11 +161,123 @@ public class AxeTransformableAxeBox extends AxeBox {
 
         // Draw the tick line
         gl.getGL2().glBegin(GL2.GL_LINES);
-        Coord3d c3d = new Coord3d(xpos, ypos, zpos);
-        GlVertexExecutor.Vertex(gl, c3d, transformers);
-        c3d = new Coord3d(xlab, ylab, zlab);
-        GlVertexExecutor.Vertex(gl, c3d, transformers);
+        GlVertexExecutor.Vertex(gl, new Coord3d(xpos, ypos, zpos), transformers);
+        GlVertexExecutor.Vertex(gl, new Coord3d(xlab, ylab, zlab), transformers);
         gl.getGL2().glEnd();
+    }*/
+    
+    protected BoundingBox3d drawTicks(GL gl, GLU glu, Camera cam, int axis, int direction, Color color, Halign hal, Valign val) {
+        int quad_0;
+        int quad_1;
+        float tickLength = 20.0f; // with respect to range
+        float axeLabelDist = 2.5f;
+        BoundingBox3d ticksTxtBounds = new BoundingBox3d();
+
+        // Retrieve the quads that intersect and create the selected axe
+        if (isX(direction)) {
+            quad_0 = axeXquads[axis][0];
+            quad_1 = axeXquads[axis][1];
+        } else if (isY(direction)) {
+            quad_0 = axeYquads[axis][0];
+            quad_1 = axeYquads[axis][1];
+        } else { // (axis==AXE_Z)
+            quad_0 = axeZquads[axis][0];
+            quad_1 = axeZquads[axis][1];
+        }
+
+        // Computes POSition of ticks lying on the selected axe
+        // (i.e. 1st point of the tick line)
+        double xpos = transformers.getX().compute(normx[quad_0]) + transformers.getX().compute(normx[quad_1]);
+        double ypos = transformers.getY().compute(normy[quad_0]) + transformers.getY().compute(normy[quad_1]);
+        double zpos = transformers.getZ().compute(normz[quad_0]) + transformers.getZ().compute(normz[quad_1]);
+
+        // Computes the DIRection of the ticks
+        // assuming initial vector point is the center
+        float xdir = (normx[quad_0] + normx[quad_1]) - center.x;
+        float ydir = (normy[quad_0] + normy[quad_1]) - center.y;
+        float zdir = (normz[quad_0] + normz[quad_1]) - center.z;
+        xdir = xdir == 0 ? 0 : xdir / Math.abs(xdir); // so that direction as
+                                                      // length 1
+        ydir = ydir == 0 ? 0 : ydir / Math.abs(ydir);
+        zdir = zdir == 0 ? 0 : zdir / Math.abs(zdir);
+
+        // Variables for storing the position of the LABel position
+        // (2nd point on the tick line)
+        double xlab;
+        double ylab;
+        double zlab;
+
+        // Draw the label for axis
+        String axeLabel;
+        int dist = 1;
+        if (isX(direction)) {
+            xlab = center.x;
+            ylab = axeLabelDist * (yrange / tickLength) * dist * ydir + ypos;
+            zlab = axeLabelDist * (zrange / tickLength) * dist * zdir + zpos;
+            axeLabel = layout.getXAxeLabel();
+        } else if (isY(direction)) {
+            xlab = axeLabelDist * (xrange / tickLength) * dist * xdir + xpos;
+            ylab = center.y;
+            zlab = axeLabelDist * (zrange / tickLength) * dist * zdir + zpos;
+            axeLabel = layout.getYAxeLabel();
+        } else {
+            xlab = axeLabelDist * (xrange / tickLength) * dist * xdir + xpos;
+            ylab = axeLabelDist * (yrange / tickLength) * dist * ydir + ypos;
+            zlab = center.z;
+            axeLabel = layout.getZAxeLabel();
+        }
+
+        drawAxisLabel(gl, glu, cam, direction, color, ticksTxtBounds, xlab, ylab, zlab, axeLabel);
+        drawAxisTicks(gl, glu, cam, direction, color, hal, val, tickLength, ticksTxtBounds, xpos, ypos, zpos, xdir, ydir, zdir, getAxisTicks(direction));
+        return ticksTxtBounds;
+    }
+    
+    public void drawAxisTicks(GL gl, GLU glu, Camera cam, int direction, Color color, Halign hal, Valign val, float tickLength, BoundingBox3d ticksTxtBounds, double xpos,
+            double ypos, double zpos, float xdir, float ydir, float zdir, double[] ticks) {
+        double xlab;
+        double ylab;
+        double zlab;
+        String tickLabel = "";
+
+        for (int t = 0; t < ticks.length; t++) {
+            // Shift the tick vector along the selected axis
+            // and set the tick length
+            if (isX(direction)) {
+                xpos = transformers.getX().compute((float) ticks[t]);
+                xlab = xpos;
+                ylab = Math.signum(tickLength * ydir) * (yrange / transformers.getY().compute(Math.abs(tickLength))) * transformers.getY().compute(Math.abs(ydir)) + ypos;
+                zlab = Math.signum(tickLength * ydir) * (zrange / transformers.getZ().compute(Math.abs(tickLength))) * transformers.getZ().compute(Math.abs(zdir)) + zpos;
+                tickLabel = layout.getXTickRenderer().format(xpos);
+            } else if (isY(direction)) {
+                ypos = transformers.getY().compute((float) ticks[t]);
+                xlab = Math.signum(tickLength * xdir) * (xrange / transformers.getX().compute(Math.abs(tickLength))) * transformers.getX().compute(Math.abs(xdir)) + xpos;
+                ylab = ypos;
+                zlab = Math.signum(tickLength * zdir) * (zrange / transformers.getZ().compute(Math.abs(tickLength))) * transformers.getZ().compute(Math.abs(zdir)) + zpos;
+                tickLabel = layout.getYTickRenderer().format(ypos);
+            } else { // (axis==AXE_Z)
+                zpos = transformers.getZ().compute((float) ticks[t]);
+                xlab = Math.signum(tickLength * xdir) * (xrange / transformers.getX().compute(Math.abs(tickLength))) * transformers.getX().compute(Math.abs(xdir)) + xpos;
+                ylab = Math.signum(tickLength * ydir) * (yrange / transformers.getY().compute(Math.abs(tickLength))) * transformers.getY().compute(Math.abs(ydir)) + ypos;
+                zlab = zpos;
+                tickLabel = layout.getZTickRenderer().format(zpos);
+            }
+            Coord3d tickPosition = new Coord3d(xlab, ylab, zlab);
+
+            if (layout.isTickLineDisplayed()) {
+                if (gl.isGL2()) {
+                    drawTickLine(gl, color, xpos, ypos, zpos, xlab, ylab, zlab);
+                } else {
+                    // FIXME REWRITE ANDROID
+                }
+            }
+
+            // Select the alignement of the tick label
+            Halign hAlign = layoutHorizontal(direction, cam, hal, tickPosition);
+            Valign vAlign = layoutVertical(direction, val, zdir);
+
+            // Draw the text label of the current tick
+            drawAxisTickNumericLabel(gl, glu, direction, cam, color, hAlign, vAlign, ticksTxtBounds, tickLabel, tickPosition);
+        }
     }
 
     protected AxeTransformerSet transformers;
