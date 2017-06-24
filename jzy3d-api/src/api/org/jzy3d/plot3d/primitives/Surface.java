89ef2db229f029cb35ff4f00958afabaece24e87
Martin Pernollet <mpernollet@octo.com>
2017-06-24 17:15:40 +0200

    refactor

---
+++ jzy3d-api/src/api/org/jzy3d/plot3d/primitives/Surface.java
@@ -0,0 +1,18 @@
+package org.jzy3d.plot3d.primitives;
+
+import org.jzy3d.colors.Color;
+import org.jzy3d.colors.ColorMapper;
+import org.jzy3d.colors.colormaps.IColorMap;
+import org.jzy3d.maths.Range;
+import org.jzy3d.plot3d.builder.Builder;
+import org.jzy3d.plot3d.builder.Mapper;
+
+public class Surface {
+    public static Shape shape(Mapper f1, Range range, int steps, IColorMap colormap) {
+        final Shape surface = Builder.buildOrthonormal(f1, range, steps);
+        surface.setColorMapper(new ColorMapper(colormap, surface.getBounds().getZmin(), surface.getBounds().getZmax(), new Color(1, 1, 1, .5f)));
+        surface.setFaceDisplayed(true);
+        surface.setWireframeDisplayed(false);
+        return surface;
+    }
+}
--- jzy3d-api/src/awt/org/jzy3d/chart/AWTChart.java
+++ jzy3d-api/src/awt/org/jzy3d/chart/AWTChart.java
@@ -2,7 +2,11 @@ package org.jzy3d.chart;
 
 import org.jzy3d.chart.factories.AWTChartComponentFactory;
 import org.jzy3d.chart.factories.IChartComponentFactory;
+import org.jzy3d.maths.Dimension;
+import org.jzy3d.plot3d.primitives.AbstractDrawable;
+import org.jzy3d.plot3d.primitives.axes.layout.IAxeLayout;
 import org.jzy3d.plot3d.rendering.canvas.Quality;
+import org.jzy3d.plot3d.rendering.legends.colorbars.AWTColorbarLegend;
 import org.jzy3d.plot3d.rendering.view.AWTView;
 import org.jzy3d.plot3d.rendering.view.Renderer2d;
 
@@ -48,5 +52,20 @@ public class AWTChart extends Chart {
 
     public AWTView getAWTView() {
         return (AWTView) view;
+    }
+    
+    public AWTColorbarLegend colorbar(AbstractDrawable drawable){
+        return colorbar(drawable, new Dimension(100, 600), getView().getAxe().getLayout());
+    }
+
+    public AWTColorbarLegend colorbar(AbstractDrawable drawable, IAxeLayout layout){
+        return colorbar(drawable, new Dimension(100, 600), layout);
+    }
+    
+    public AWTColorbarLegend colorbar(AbstractDrawable drawable, Dimension d, IAxeLayout layout){
+        AWTColorbarLegend cbar = new AWTColorbarLegend(drawable, layout);
+        cbar.setMinimumSize(d);
+        drawable.setLegend(cbar);
+        return cbar;
     }
 }
