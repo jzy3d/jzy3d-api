931874d07010b7ff7dc5899a392b13e85f719f84
Hannes Wellmann <Hannes@ICE-PC.fritz.box>
2021-04-02 20:36:17 +0200

    Clean-up IO classes

--- jzy3d-core/src/main/java/org/jzy3d/io/FileReader.java
+++ jzy3d-core/src/main/java/org/jzy3d/io/FileReader.java
@@ -1,25 +1,16 @@
 package org.jzy3d.io;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 
+@Deprecated(since = "2.0.0", forRemoval = true)
 public class FileReader {
-  public static List<String> read(String file) throws IOException {
-    FileInputStream fstream = new FileInputStream(file);
-    DataInputStream in = new DataInputStream(fstream);
-    BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
-    List<String> out = new ArrayList<String>();
-    String strLine;
-    while ((strLine = br.readLine()) != null) {
-      out.add(strLine);
-    }
-    in.close();
-    return out;
+  /**
+   * @deprecated use {@link SimpleFile#read(String)} instead
+   */
+  @Deprecated(since = "2.0.0", forRemoval = true)
+  public static List<String> read(String file) throws IOException {
+    return SimpleFile.read(file);
   }
 }
--- jzy3d-core/src/main/java/org/jzy3d/io/SimpleDir.java
+++ jzy3d-core/src/main/java/org/jzy3d/io/SimpleDir.java
@@ -2,90 +2,52 @@
 package org.jzy3d.io;
 
 import java.io.File;
-import java.io.FileFilter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class SimpleDir {
 
-  public SimpleDir() {}
+  private SimpleDir() {}
 
   public static List<File> getAllFolders(File file) throws IOException {
-    if (!file.exists())
-      throw new IOException(
-          (new StringBuilder()).append("File does not exist:").append(file).toString());
-    List<File> output = new ArrayList<File>();
-    File folders[] = listDir(file);
-    File arr$[] = folders;
-    int len$ = arr$.length;
-    for (int i$ = 0; i$ < len$; i$++) {
-      File f = arr$[i$];
-      output.add(f);
+    if (!file.exists()) {
+      throw new IOException("File does not exist:" + file);
     }
-
+    List<File> output = new ArrayList<>();
+    File[] folders = listDir(file);
+    Collections.addAll(output, folders);
     return output;
   }
 
   public static File[] listFile(File dir) {
-    FileFilter fileFilter = new FileFilter() {
-
-      @Override
-      public boolean accept(File file) {
-        return !file.isDirectory();
-      }
-
-    };
-    return dir.listFiles(fileFilter);
+    return dir.listFiles(File::isFile);
   }
 
   public static File[] listDir(File dir) {
-    FileFilter fileFilter = new FileFilter() {
-
-      @Override
-      public boolean accept(File file) {
-        return file.isDirectory();
-      }
-
-    };
-    return dir.listFiles(fileFilter);
+    return dir.listFiles(File::isDirectory);
   }
 
-  public static List<File> getAllFiles(List<File> file) throws IOException {
-    if (file.size() == 0)
-      return new ArrayList<File>(0);
-    if (file.size() == 1)
-      return getAllFiles(file.get(0));
-    List<File> out = new ArrayList<File>();
-    File f;
-    for (Iterator<File> i$ = file.iterator(); i$.hasNext(); out.addAll(getAllFiles(f)))
-      f = i$.next();
-
+  public static List<File> getAllFiles(List<File> files) throws IOException {
+    if (files.isEmpty()) {
+      return Collections.emptyList();
+    }
+    if (files.size() == 1) {
+      return getAllFiles(files.get(0));
+    }
+    List<File> out = new ArrayList<>();
+    for (File f : files) {
+      out.addAll(getAllFiles(f));
+    }
     return out;
   }
 
   public static List<File> getAllFiles(File file) throws IOException {
-    if (!file.exists())
-      throw new IOException(
-          (new StringBuilder()).append("File does not exist:").append(file).toString());
-    File files[] = listFile(file);
-    File folders[] = listDir(file);
-    List<File> out = new ArrayList<File>();
-    File arr$[] = files;
-    int len$ = arr$.length;
-    for (int i$ = 0; i$ < len$; i$++) {
-      File f = arr$[i$];
-      out.add(f);
+    if (!file.exists()) {
+      throw new IOException("File does not exist:" + file);
     }
-
-    arr$ = folders;
-    len$ = arr$.length;
-    for (int i$ = 0; i$ < len$; i$++) {
-      File f = arr$[i$];
-      out.addAll(getAllFiles(f));
-    }
-
-    return out;
+    return Collections.unmodifiableList(Arrays.asList(file.listFiles()));
   }
 }
