15bff53d8eaf7fa600baf3951123761afa441aca
Martin Pernollet <martin@jzy3d.org>
2021-05-25 12:29:37 +0200

    Adding ITTest for disks

---
+++ jzy3d-emul-gl/src/test/java/org/jzy3d/emulgl/integration/ITTestEmulGLDisk.java
@@ -0,0 +1,32 @@
+package org.jzy3d.emulgl.integration;
+
+import org.junit.Test;
+import org.jzy3d.chart.Chart;
+import org.jzy3d.chart.factories.EmulGLChartFactory;
+import org.jzy3d.colors.Color;
+import org.jzy3d.junit.ChartTester;
+import org.jzy3d.maths.Coord3d;
+import org.jzy3d.plot3d.primitives.Disk;
+import org.jzy3d.plot3d.rendering.canvas.Quality;
+import org.jzy3d.utils.LoggerUtils;
+
+public class ITTestEmulGLDisk {
+  @Test
+  public void whenDiskChart_ThenMatchBaselineImagePixelwise() {
+    LoggerUtils.minimal();
+
+    // Given
+    EmulGLChartFactory factory = new EmulGLChartFactory();
+    Chart chart = factory.newChart(Quality.Advanced().setHiDPIEnabled(false));
+
+    // When
+    chart.add(new Disk(new Coord3d(0,0,00), 3.f, 11.f, 24, 5, Color.BLUE));
+    chart.add(new Disk(new Coord3d(0,0,10), 3.f, 11.f, 24, 5, Color.RED));
+
+    // Then
+    ChartTester tester = new ChartTester();
+    tester.assertSimilar(chart,
+        ChartTester.EXPECTED_IMAGE_FOLDER + this.getClass().getSimpleName() + ".png");
+  }
+
+}
