a0ac156867d4ca9918dc20b4a2490ee687ebd866
Martin Pernollet <martin@jzy3d.org>
2021-10-19 08:05:01 +0200

    coplanar manager can mount sub elements

--- jzy3d-core/src/main/java/org/jzy3d/plot3d/primitives/CoplanarityManager.java
+++ jzy3d-core/src/main/java/org/jzy3d/plot3d/primitives/CoplanarityManager.java
@@ -1,6 +1,8 @@
 package org.jzy3d.plot3d.primitives;
 
 import java.util.List;
+import org.jzy3d.io.IGLLoader;
+import org.jzy3d.maths.BoundingBox3d;
 import org.jzy3d.maths.Lists;
 import org.jzy3d.painters.IPainter;
 import org.jzy3d.painters.StencilFunc;
@@ -8,18 +10,20 @@ import org.jzy3d.painters.StencilOp;
 import org.jzy3d.plot3d.transform.Transform;
 
 /**
- * This pseudo drawable will handle drawing outline, contour line, or text that may be coplanar or partially hidden by a collection of polygons. 
+ * This pseudo drawable will handle drawing outline, contour line, or text that may be coplanar or
+ * partially hidden by a collection of polygons.
  * 
- * This allows rendering outlines properly and hence deals with Z-fighting artifacts, 2d text crossing 3D contents, etc.
+ * This allows rendering outlines properly and hence deals with Z-fighting artifacts, 2d text
+ * crossing 3D contents, etc.
  * 
  * @author Martin Pernollet
  *
  */
-public class CoplanarityManager extends Drawable{
+public class CoplanarityManager extends Drawable implements IGLBindedResource{
   protected Drawable outline;
   protected List<Drawable> outlines;
   protected Drawable plane;
-  
+
   public CoplanarityManager(Drawable outline, Drawable plane) {
     this(Lists.of(outline), plane);
   }
@@ -36,46 +40,87 @@ public class CoplanarityManager extends Drawable{
    */
   @Override
   public void draw(IPainter painter) {
-    
+
+    // Turn on stenciling
     painter.glEnable_Stencil();
-    
+
+    // Set stencil function to always pass
     painter.glStencilFunc(StencilFunc.GL_ALWAYS, 1, 1);
+    
+    // Set stencil op to set 1 if depth passes, 0 if it fails
     painter.glStencilOp(StencilOp.GL_KEEP, StencilOp.GL_ZERO, StencilOp.GL_REPLACE);
-    
+
+    // Draw the base polygons
     plane.draw(painter);
-    
+
+    // Set stencil function to pass when stencil is 1
     painter.glStencilFunc(StencilFunc.GL_EQUAL, 1, 1);
-    
+
+    // Disable writes to stencil buffer
     painter.glStencilMask_False();
+    
+    // Turn off depth buffering
     painter.glDisable_DepthTest();
-    
-    //outline.draw(painter);
-    
-    for(Drawable d: outlines) {
+
+    // Render the overlying drawables
+    for (Drawable d : outlines) {
       d.draw(painter);
     }
-    
+
     // Reset states
-    if(!painter.getQuality().isAlphaActivated()) {
-      if(painter.getQuality().isDepthActivated()) {
+    if (!painter.getQuality().isAlphaActivated()) {
+      if (painter.getQuality().isDepthActivated()) {
         painter.glEnable_DepthTest();
       }
+    } else if (!painter.getQuality().isDisableDepthBufferWhenAlpha()) {
+      painter.glEnable_DepthTest();
     }
-    else if(!painter.getQuality().isDisableDepthBufferWhenAlpha()){
-        painter.glEnable_DepthTest();
-    }
-    
+
     painter.glDisable_Stencil();
-    
-    // could also try https://www.khronos.org/opengl/wiki/Drawing_Coplanar_Primitives_Widthout_Polygon_Offset
+
+    // could also try
+    // https://www.khronos.org/opengl/wiki/Drawing_Coplanar_Primitives_Widthout_Polygon_Offset
   }
 
   @Override
-  public void applyGeometryTransform(Transform transform) {
-  }
+  public void applyGeometryTransform(Transform transform) {}
 
   @Override
   public void updateBounds() {
+    for(Drawable outline : outlines) {
+      outline.updateBounds();
+    }
+    plane.updateBounds();
+    
+    bbox.reset();
+    for(Drawable outline : outlines) {
+      bbox.add(outline);
+    }
+    bbox.add(plane);
+  }
+
+  @Override
+  public void mount(IPainter painter) {
+    mount(painter, plane);
+    
+    for(Drawable d: outlines) {
+      mount(painter, d);
+    }
+    
+    mounted = true;
+  }
+  
+  boolean mounted = false;
+  
+  protected void mount(IPainter painter, Drawable d) {
+    if(d instanceof IGLBindedResource) {
+      ((IGLBindedResource) d).mount(painter);
+    }    
+  }
+
+  @Override
+  public boolean hasMountedOnce() {
+    return mounted;
   }
 
 }
