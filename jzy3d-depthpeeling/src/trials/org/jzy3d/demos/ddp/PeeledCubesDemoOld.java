34d3b3d865b1bbe7238e076a1262285eab92fba5
Martin Pernollet <martin@jzy3d.org>
2021-12-02 15:31:26 +0100

    cleanup

--- jzy3d-depthpeeling/src/trials/org/jzy3d/demos/ddp/PeeledCubesDemoOld.java
+++ jzy3d-depthpeeling/src/trials/org/jzy3d/demos/ddp/PeeledCubesDemoOld.java
@@ -2,61 +2,70 @@ package org.jzy3d.demos.ddp;
 
 import org.jzy3d.chart.Chart;
 import org.jzy3d.chart.ChartLauncher;
-import org.jzy3d.chart.factories.NativePainterFactory;
 import org.jzy3d.colors.Color;
-import org.jzy3d.factories.DepthPeelingChartFactory;
-import org.jzy3d.factories.DepthPeelingPainterFactory;
 import org.jzy3d.maths.BoundingBox3d;
 import org.jzy3d.maths.Coord3d;
 import org.jzy3d.maths.Rectangle;
 import org.jzy3d.plot3d.primitives.ParallelepipedComposite;
 import org.jzy3d.plot3d.primitives.ParallelepipedComposite.PolygonType;
 import org.jzy3d.plot3d.primitives.PolygonMode;
+import org.jzy3d.plot3d.rendering.canvas.Quality;
+import org.jzy3d.plot3d.rendering.ddp.DepthPeelingChart;
 import org.jzy3d.plot3d.rendering.ddp.algorithms.PeelingMethod;
-import com.jogamp.opengl.GLCapabilities;
-import com.jogamp.opengl.GLProfile;
+
 
 /**
- * WEIGHTED_SUM_MODE
- * no compilation problem BUT overlapping translucent part are black
  * 
  * 
- * WEIGHTED_AVERAGE_MODE
- * extension 'ARB_draw_buffers' is not supported - despite available
- * '-' does not operate on 'int' and 'float'
  * 
- * DUAL_PEELING_MODE
- * extension 'ARB_draw_buffers' is not supported - despite available
- * '==' does not operate on 'float' and 'int'
+ * -------------------------
+ * no pb avec simple polygon:
+ * <li>gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
+ * <li>gl.glEnable(GL2.GL_POLYGON_OFFSET_FILL);
  * 
+ * Probleme avec polygon normal > probleme enleve si p1.setWireframeDisplayed(false); > l'ordre dans
+ * le scene graph n'a pas l'air d'avoir d'impact
  * 
- * F2B_PEELING_MODE
- * '+' does not operate on 'vec4' and 'vec3'
-
+ * Probleme avec simple polygon qd create stack avant ou apres
  * 
  * 
- * @author martin
- *
+ * Dans la vue decomposee renderer.display, le rendering devient tres lent si
+ * view.renderOverlay(gl); n'est pas appele
  */
-public class PeeledCubesDemo_2 {
+public class PeeledCubesDemoOld {
   public static void main(String[] args) {
-
-    GLProfile profile = GLProfile.get(GLProfile.GL4);
-    GLCapabilities caps = NativePainterFactory.getOffscreenCapabilities(profile);
-    DepthPeelingPainterFactory p = new DepthPeelingPainterFactory(caps);
-    DepthPeelingChartFactory f = new DepthPeelingChartFactory(PeelingMethod.WEIGHTED_SUM_MODE);
-    Chart chart = f.newChart();
+    Chart chart = DepthPeelingChart.get(Quality.Fastest(), "awt", PeelingMethod.WEIGHTED_SUM_MODE);
     chart.getView().setAxisDisplayed(false);
-    // chart.setAnimated(false);
+chart.setAnimated(false);
+
+    
 
     createStack(chart, 0.01f, 0.01f, Coord3d.ORIGIN, Color.BLUE /* no alpha */, Color.BLACK);
     createStack(chart, 0.01f, 0.01f, new Coord3d(0.005f, 0.005f, 0.005f),
         new Color(1f, 0f, 0f, 0.5f), Color.BLACK);
     createStack(chart, 0.01f, 0.01f, new Coord3d(0.01f, 0.01f, 0.01f), new Color(0f, 1f, 0f, 0.5f),
         Color.BLACK);
+
     
+    chart.open(800, 600);
+    chart.getMouse();
     
-    ChartLauncher.openChart(chart, new Rectangle(0, 0, 600, 600));
+    String info = chart.getCanvas().getDebugInfo();
+    
+    if(!info.contains("ARB_texture_rectangle")) {
+      System.out.println(info);      
+    }
+    else {
+      System.out.println("ARB_texture_rectangle is here!!!");
+    }
+    
+    if(!info.contains("ARB_draw_buffers")) {
+      System.out.println(info);      
+    }
+    else {
+      System.out.println("ARB_draw_buffers is here!!!");
+
+    }
   }
 
   public static void createStack(Chart chart, float width, float height, Coord3d position,
--- jzy3d-native-jogl-core/src/main/java/org/jzy3d/io/obj/OBJFileLoader.java
+++ jzy3d-native-jogl-core/src/main/java/org/jzy3d/io/obj/OBJFileLoader.java
@@ -1,5 +1,6 @@
 package org.jzy3d.io.obj;
 
+import java.io.File;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import org.apache.log4j.Logger;
@@ -13,19 +14,19 @@ import com.jogamp.common.nio.Buffers;
 public class OBJFileLoader implements IGLLoader<DrawableVBO> {
   static Logger logger = Logger.getLogger(OBJFileLoader.class);
 
-  protected String filename;
+  protected File file;
   protected OBJFile obj;
 
-  public OBJFileLoader(String filename) {
-    this.filename = filename;
+  public OBJFileLoader(File file) {
+    this.file = file;
   }
 
   @Override
   public void load(IPainter painter, DrawableVBO drawable) {
     obj = new OBJFile();
 
-    logger.info("Start loading OBJ file '" + filename + "'");
-    obj.loadModelFromFilename(filename);
+    logger.info("Start loading OBJ file '" + file.getAbsolutePath() + "'");
+    obj.loadModelFromFile(file);
 
     logger.info("Start compiling mesh");
     obj.compileModel();
