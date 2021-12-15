e3e325e539ea24939f1c61e99fdbe4ec1b81efc8
Martin Pernollet <martin@jzy3d.org>
2021-12-15 17:25:11 +0100

    Update status

--- jzy3d-depthpeeling/src/main/java/org/jzy3d/plot3d/rendering/ddp/algorithms/PeelingMethod.java
+++ jzy3d-depthpeeling/src/main/java/org/jzy3d/plot3d/rendering/ddp/algorithms/PeelingMethod.java
@@ -1,25 +1,25 @@
 package org.jzy3d.plot3d.rendering.ddp.algorithms;
 
 /**
- * Status of peeling methods on Apple M1 (Silicon)
+ * Status of peeling methods on {macOS 10.12 + NVidia GPU} {Ubuntu 20.04 + Intel Iris GPU}
  * <ul>
- * <li>OK - DUAL_PEELING_MODE 
- * <li>KO - WEIGHTED_AVERAGE_MODE : renders correctly BUT make opaque object appear translucent (e.g. the
- * blue cube of this demo)
- * <li>KO - WEIGHTED_SUM_MODE : no compilation problem BUT overlapping parts (translucent or opaque) are
- * black
+ * <li>OK - F2B_PEELING_MODE
+ * <li>OK - DUAL_PEELING_MODE
+ * <li>OK - WEIGHTED_AVERAGE_MODE
+ * <li>KO - WEIGHTED_SUM_MODE : no compilation problem BUT overlapping parts (translucent or opaque)
+ * are black
+ * </ul>
+ * 
+ * Status of peeling methods on macOS 11.4 + M1 (Silicon)
+ * <ul>
+ * <li>OK - DUAL_PEELING_MODE
+ * <li>KO - WEIGHTED_AVERAGE_MODE : renders correctly BUT make opaque object appear translucent
+ * (e.g. the blue cube of this demo)
+ * <li>KO - WEIGHTED_SUM_MODE : no compilation problem BUT overlapping parts (translucent or opaque)
+ * are black
  * <li>KO - F2B_PEELING_MODE : Hangs before display (reproduce with chart.get
  * </ul>
- * 
- * Status of peeling methods on NVidia
- * <ul>
- * <li>OK - F2B_PEELING_MODE 
- * <li>OK - DUAL_PEELING_MODE 
- * <li>OK - WEIGHTED_AVERAGE_MODE
- * <li>KO - WEIGHTED_SUM_MODE : no compilation problem BUT overlapping parts (translucent or opaque) are
- * black
- * </ul>
- * 
+ *
  * @author martin
  *
  */
--- jzy3d-depthpeeling/src/test/java/org/jzy3d/demos/ddp/PeeledCubesDemo.java
+++ jzy3d-depthpeeling/src/test/java/org/jzy3d/demos/ddp/PeeledCubesDemo.java
@@ -16,17 +16,7 @@ import com.jogamp.opengl.GLContext;
 import com.jogamp.opengl.awt.GLCanvas;
 
 /**
- * Status of peeling methods on Apple M1 (Silicon)
- * <ul>
- * <li>OK - DUAL_PEELING_MODE
- * <li>KO - WEIGHTED_AVERAGE_MODE : renders correctly BUT make opaque object appear translucent
- * (e.g. the blue cube of this demo)
- * <li>KO - WEIGHTED_SUM_MODE : no compilation problem BUT overlapping parts (translucent or opaque)
- * are black
- * <li>KO - F2B_PEELING_MODE : Hangs before display (reproduce with chart.get
- * </ul>
- * 
- * Status of peeling methods on NVidia
+ * Status of peeling methods on {macOS 10.12 + NVidia GPU} {Ubuntu 20.04 + Intel Iris GPU}
  * <ul>
  * <li>OK - F2B_PEELING_MODE
  * <li>OK - DUAL_PEELING_MODE
@@ -35,13 +25,23 @@ import com.jogamp.opengl.awt.GLCanvas;
  * are black
  * </ul>
  * 
+ * Status of peeling methods on macOS 11.4 + M1 (Silicon)
+ * <ul>
+ * <li>OK - DUAL_PEELING_MODE
+ * <li>KO - WEIGHTED_AVERAGE_MODE : renders correctly BUT make opaque object appear translucent
+ * (e.g. the blue cube of this demo)
+ * <li>KO - WEIGHTED_SUM_MODE : no compilation problem BUT overlapping parts (translucent or opaque)
+ * are black
+ * <li>KO - F2B_PEELING_MODE : Hangs before display (reproduce with chart.get
+ * </ul>
+ *
  * @author martin
  *
  */
 public class PeeledCubesDemo {
   public static void main(String[] args) throws InterruptedException, IOException {
     DepthPeelingPainterFactory p = new DepthPeelingPainterFactory();
-    p.setPeelingMethod(PeelingMethod.WEIGHTED_AVERAGE_MODE);
+    p.setPeelingMethod(PeelingMethod.DUAL_PEELING_MODE);
 
     AWTChartFactory f = new AWTChartFactory(p);
     Chart chart = f.newChart();
