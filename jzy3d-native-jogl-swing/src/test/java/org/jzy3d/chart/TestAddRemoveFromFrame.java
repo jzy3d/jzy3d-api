a538367ae6b8fe780bc041ccb0e847c5bc900128
martin <martin@jzy3d.org>
2022-02-18 14:38:13 +0100

    Test uses another text renderer to avoid crash

--- jzy3d-native-jogl-swing/src/test/java/org/jzy3d/chart/TestAddRemoveFromFrame.java
+++ jzy3d-native-jogl-swing/src/test/java/org/jzy3d/chart/TestAddRemoveFromFrame.java
@@ -6,9 +6,68 @@ import org.junit.Test;
 import org.jzy3d.chart.factories.AWTChartFactory;
 import org.jzy3d.plot3d.primitives.SampleGeom;
 import org.jzy3d.plot3d.rendering.canvas.Quality;
+import org.jzy3d.plot3d.text.renderers.TextBitmapRenderer;
 import com.jogamp.opengl.awt.GLCanvas;
 
 public class TestAddRemoveFromFrame {
+  
+  int PAUSE = 1000;
+  
+  @Test
+  public void addRemove_AWTCanvas_from_AWTFrame() {
+
+    // Given : a chart added to an application frame
+    AWTChartFactory factory = new AWTChartFactory();
+    
+    Quality q = Quality.Advanced().setAnimated(true);
+    
+    Chart chart = factory.newChart(q);
+    chart.add(SampleGeom.surface());
+    
+//    chart.getView().getAxis().setTextRenderer(new TextBitmapRenderer());
+
+    Frame frame = new Frame();
+    frame.add((java.awt.Component)chart.getCanvas());
+    frame.pack();
+    frame.setBounds(0,0,300,300);
+    frame.setVisible(true);
+    
+    // -------------------------------------------------
+    // Given : we queried a repaint and left some time to AWT
+    // to achieve it before we start removing the component
+    
+    chart.render();
+    System.out.println("Should appear");
+    chart.sleep(PAUSE);
+
+    // -------------------------------------------------
+    // When : removing chart from the application frame
+    
+    frame.remove((java.awt.Component)chart.getCanvas());    
+    System.out.println("Should disappear");
+
+    for (int i = 0; i < 250; i++) {
+      //chart.render();
+      ((GLCanvas)chart.getCanvas()).display();
+    }
+    chart.sleep(PAUSE);    
+    
+    // Then no exception should occur
+
+    // -------------------------------------------------
+    // When adding the chart again
+    
+    frame.add((java.awt.Component)chart.getCanvas());
+    System.out.println("Should re-appear");
+
+    for (int i = 0; i < 250; i++) {
+      //chart.render();
+      ((GLCanvas)chart.getCanvas()).display();
+    }
+    chart.sleep(PAUSE);
+
+  }
+  
   //@Test
   public void addRemove_AWTCanvas_from_SwingFrame() {
     
@@ -36,57 +95,7 @@ public class TestAddRemoveFromFrame {
     chart.sleep(PAUSE);
 
   }
-  
-  int PAUSE = 1000;
-  
-  @Test
-  public void addRemove_AWTCanvas_from_AWTFrame() {
 
-    // Given : a chart added to an application frame
-    AWTChartFactory factory = new AWTChartFactory();
-    
-    Quality q = Quality.Advanced().setAnimated(true);
-    
-    Chart chart = factory.newChart(q);
-    chart.add(SampleGeom.surface());
-
-    Frame frame = new Frame();
-    frame.add((java.awt.Component)chart.getCanvas());
-    frame.pack();
-    frame.setBounds(0,0,300,300);
-    frame.setVisible(true);
-    
-    // -------------------------------------------------
-    // Given we queried a repaint and left some time to AWT
-    // to achieve it before we start removing the component
-    
-    chart.render();
-    System.out.println("Should appear");
-    chart.sleep(PAUSE);
-
-    // -------------------------------------------------
-    // When : removing chart from the application frame
-    
-    frame.remove((java.awt.Component)chart.getCanvas());    
-    System.out.println("Should disappear");
-
-    for (int i = 0; i < 250; i++) {
-      //chart.render();
-      ((GLCanvas)chart.getCanvas()).display();
-    }
-    chart.sleep(PAUSE);    
-    
-    // Then no exception should occur
-
-    // -------------------------------------------------
-    // When adding the chart again
-    
-    frame.add((java.awt.Component)chart.getCanvas());
-    System.out.println("Should re-appear");
-    chart.render();
-    chart.sleep(PAUSE);
-
-  }
   
   //@Test
   public void addRemove_AWTCanvas_from_Nowhere() {
