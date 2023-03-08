5fd128bf2f7020d8135c56bc858defe2ad87b1ae
martin <martin@jzy3d.org>
2023-03-08 17:03:13 +0100

    an awt painter for swing node

---
+++ jzy3d-native-jogl-javafx-jdk19/src/main/java/org/jzy3d/javafx/swing/JavaFXAWTPainterFactory.java
@@ -0,0 +1,5 @@
+package org.jzy3d.javafx.swing;
+
+import org.jzy3d.chart.factories.AWTPainterFactory;
+
+public class JavaFXAWTPainterFactory extends AWTPainterFactory{}
--- jzy3d-native-jogl-javafx-jdk19/src/main/java/org/jzy3d/javafx/swing/JavaFXSwingChartFactory.java
+++ jzy3d-native-jogl-javafx-jdk19/src/main/java/org/jzy3d/javafx/swing/JavaFXSwingChartFactory.java
@@ -1,9 +1,13 @@
 package org.jzy3d.javafx.swing;
 
+import java.awt.Component;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 import org.jzy3d.chart.Chart;
+import org.jzy3d.chart.factories.IPainterFactory;
 import org.jzy3d.chart.factories.SwingChartFactory;
 import org.jzy3d.javafx.controllers.mouse.JavaFXCameraMouseController;
+import org.jzy3d.plot3d.rendering.canvas.ICanvas;
 import javafx.embed.swing.SwingNode;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -12,14 +16,32 @@ import javafx.stage.Stage;
 public class JavaFXSwingChartFactory extends SwingChartFactory{
 
   public JavaFXSwingChartFactory() {
-    super(new JavaFXSwingPainterFactory());
+    this(new JavaFXSwingPainterFactory());
   }
   
+  public JavaFXSwingChartFactory(IPainterFactory painterFactory) {
+    super(painterFactory);
+  }
+
+
+
   public void open(Chart chart, Stage stage) {
     // JavaFX
     SwingNode swingNode = new SwingNode();
-    //swingNode.setContent(jscrollPane);
-    swingNode.setContent((JPanel)chart.getCanvas());
+
+    // Fill node with canvas
+    ICanvas canvas = chart.getCanvas();
+
+    if(canvas instanceof JComponent)
+      swingNode.setContent((JComponent)canvas);
+    else if(canvas instanceof Component) {
+      JPanel embedding = new JPanel();
+      embedding.add((Component)canvas);
+      swingNode.setContent(embedding);
+
+    }
+      
+    
     
     // Camera mouse
     JavaFXCameraMouseController mouse = new JavaFXCameraMouseController(chart);
